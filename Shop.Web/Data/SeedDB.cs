6ae1bbedaac9fb9cd312cf3f636c9900f5d15d94
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-06-06 14:08:52 -0500

    Add users

--- Shop.Web/Data/SeedDB.cs
+++ Shop.Web/Data/SeedDB.cs
@@ -4,14 +4,18 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
+    using Microsoft.AspNetCore.Identity;
+
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly UserManager<User> userManager;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
             this.random = new Random();
         }
 
@@ -19,23 +23,45 @@
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userManager.FindByEmailAsync("gabrielpanta1234@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Gabriel",
+                    LastName = "Panta Jimenez",
+                    Email = "gabrielpanta1234@gmail.com",
+                    UserName = "gabrielpanta1234@gmail.comm",
+                    PhoneNumber = "969060448"
+                    
+                };
+
+                var result = await this.userManager.CreateAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("Iphone X");
-                this.AddProduct("Magic Mouse");
-                this.AddProduct("iWatch");
+                this.AddProduct("Iphone X",user);
+                this.AddProduct("Magic Mouse",user);
+                this.AddProduct("iWatch",user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name,User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
                 Price = this.random.Next(1000),
                 IsAvailabe = true,
-                Stock = this.random.Next(100)
+                Stock = this.random.Next(100),
+                User=user
             });
         }
     }
