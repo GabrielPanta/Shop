0c3b6662aa84959a9c209baa2c38b48794f219c3
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-08-12 12:42:10 -0500

    Tutorial 57 - Part 21 - Login View Model

--- Shop.UIForms/Shop.UIForms/Infrastructure/InstanceLocator.cs
+++ Shop.UIForms/Shop.UIForms/Infrastructure/InstanceLocator.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Shop.UIForms.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Shop.UIForms.Infrastructure
 {
-    class InstanceLocator
+    public class InstanceLocator
     {
+        public MainViewModel Main { get; set; }
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
     }
 }
